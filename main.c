9624aef9d83469405e728b510bcfafa6b1baea81
olvvvadamilola <destric3@gmail.com>
2023-12-13 02:30:49 +0100

    main.c created

---
+++ main.c
@@ -0,0 +1,20 @@
+#include "monty.h"
+
+stack_t *h = NULL;
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    free_node();
+    return (0);
+}
