c6f1e1ea80e91797613a16566b9629de7125e0a3
olvvvadamilola <destric3@gmail.com>
2023-12-15 01:20:06 +0100

    check

--- 2_nodes.c
+++ 2_nodes.c
@@ -38,3 +38,28 @@ void free_nodes(void)
 		free(tmp);
 	}
 }
+
+/**
+ * queue - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ * @ln: line number of the opcode.
+ */
+void queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+
+}
--- stack1.c
+++ stack1.c
@@ -23,31 +23,6 @@ void stack_add(stack_t **new_node, __attribute__((unused))unsigned int ln)
 	tmp->prev = head;
 }
 
-/**
- * queue - Adds a node to the queue.
- * @new_node: Pointer to the new node.
- * @ln: line number of the opcode.
- */
-void queue(stack_t **new_node, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (new_node == NULL || *new_node == NULL)
-		exit(EXIT_FAILURE);
-	if (head == NULL)
-	{
-		head = *new_node;
-		return;
-	}
-	tmp = head;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = *new_node;
-	(*new_node)->prev = tmp;
-
-}
-
 
 /**
  * stack_print - Adds a node to the stack.
