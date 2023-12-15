9daf472f118d90076636af60ba11d44933b950b0
olvvvadamilola <destric3@gmail.com>
2023-12-15 03:14:05 +0100

    check

--- stack_strings.c
+++ stack_strings.c
@@ -7,15 +7,20 @@
  */
 void p_char(stack_t **stack, unsigned int line_number)
 {
-	int ascii;
+	int value;
 
 	if (stack == NULL || *stack == NULL)
 		string_err(11, line_number);
 
-	ascii = (*stack)->n;
-	if (ascii < 0 || ascii > 127)
+	if (value >= 0 && value <= 127)
+	{
+		printf("%c\n", (char)value);
+	}
+	else
+	{
 		string_err(10, line_number);
-	printf("%c\n", ascii);
+	}
+	printf("%c\n", value);
 }
 
 /**
@@ -44,52 +49,4 @@ void p_str(stack_t **stack, __attribute__((unused))unsigned int ln)
 		tmp = tmp->next;
 	}
 	printf("\n");
-}
-
-/**
- * rotl - Rotates the first node of the stack to the bottom.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @ln: Interger representing the line number of of the opcode.
- */
-void rotl(stack_t **stack, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		return;
-
-	tmp = *stack;
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = *stack;
-	(*stack)->prev = tmp;
-	*stack = (*stack)->next;
-	(*stack)->prev->next = NULL;
-	(*stack)->prev = NULL;
-}
-
-
-/**
- * rotr - Rotates the last node of the stack to the top.
- * @stack: Pointer to a pointer pointing to top node of the stack.
- * @ln: Interger representing the line number of of the opcode.
- */
-void rotr(stack_t **stack, __attribute__((unused))unsigned int ln)
-{
-	stack_t *tmp;
-
-	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		return;
-
-	tmp = *stack;
-
-	while (tmp->next != NULL)
-		tmp = tmp->next;
-
-	tmp->next = *stack;
-	tmp->prev->next = NULL;
-	tmp->prev = NULL;
-	(*stack)->prev = tmp;
-	(*stack) = tmp;
 }
